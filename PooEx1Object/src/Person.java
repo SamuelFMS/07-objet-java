6d8e819babd2594f8583731aa1fa76a4d4851eb1
Samuel Curran <samuel.curran@fms-ea.com>
2026-08-17 11:00:43 +0200

    Creation des constructeurs pour la classe Person

--- PooEx1Object/src/Person.java
+++ PooEx1Object/src/Person.java
@@ -1,2 +1,25 @@
 public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private  String address;
+
+    public Person(String firstName, String lastName, int age, String address){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.address = address;
+    }
+
+    public Person(String firstName, String lastName, int age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Person(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
 }
