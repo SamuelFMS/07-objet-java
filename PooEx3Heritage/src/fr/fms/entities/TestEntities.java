046397b66295411a291ddb33d165cc4fc7670646
Samuel Curran <samuel.curran@fms-ea.com>
2026-08-17 13:49:39 +0200

    liste d’objets pouvant contenir des employés ou des commerciaux

--- PooEx3Heritage/src/fr/fms/entities/TestEntities.java
+++ PooEx3Heritage/src/fr/fms/entities/TestEntities.java
@@ -1,10 +1,18 @@
 package fr.fms.entities;
 
+import java.util.ArrayList;
+
 public class TestEntities {
     public static void main(String[] args) {
+        ArrayList<Person> myEmployeesAndCommercials = new ArrayList<Person>();
         Employee billGate = new Employee("Gate", "Bill", 65, "USA", new Capital("Washington", "USA", "W-M"),"Fondation B&M", 100000);
         Employee musk = new Employee("Musk", "Elon", 49, "palo alto", new City("Pretoria", "Affrique du Sud", 800000), "Tesla", 150000);
         Commercial dupont = new Commercial("Dupont", "robert", 50, "rue des rosiers à Toulouse", new City("Limoges", "France", 133000), "brico", 5);
-        System.out.println(dupont);
+        myEmployeesAndCommercials.add(billGate);
+        myEmployeesAndCommercials.add(musk);
+        myEmployeesAndCommercials.add(dupont);
+        for(Person employeeOrCommercial: myEmployeesAndCommercials) {
+            System.out.println(employeeOrCommercial);
+        }
     }
 }
