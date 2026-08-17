71e3933baeddcde83e87e725743308746e321bab
Samuel Curran <samuel.curran@fms-ea.com>
2026-08-17 10:23:47 +0200

    Creation de la methode d'initialisation de la classe City

--- PooEx1Object/src/City.java
+++ PooEx1Object/src/City.java
@@ -1,2 +1,11 @@
 public class City {
+    private String city;
+    private String country;
+    private int nbInhabitant;
+
+    public City(String city, String country, int nbInhabitant){
+        this.city = city;
+        this.country = country;
+        this.nbInhabitant = nbInhabitant;
+    }
 }
