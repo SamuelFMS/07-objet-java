3ca9f2ee63b7297e1bc0ddf2261489847073260c
Samuel Curran <samuel.curran@fms-ea.com>
2026-08-17 12:00:58 +0200

    Ajout de la classe Person

---
+++ PooEx3Heritage/src/fr/fms/entities/Person.java
@@ -0,0 +1,54 @@
+package fr.fms.entities;
+
+public class Person {
+    private final String lastName;
+    private final String firstName;
+    private int age;
+    private String address;
+    private City bornCity;
+
+    public Person(String lastName, String firstName, int age, String address, City bornCity) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.address = address;
+        this.bornCity = bornCity;
+    }
+
+    public Person(String lastName, String firstName, int age, String address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.address = address;
+    }
+
+    public Person(String lastName, String firstName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Person(String lastName, String firstName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public boolean checkIfAddressContain(String address) {
+        return this.address.contains(address);
+    }
+
+    public boolean checkIfBornInCountry(String country){
+        return this.bornCity.getCountry().equals(country);
+    }
+
+    public void display(){
+        System.out.print(lastName + ", " + firstName + ", "+ age + "ans, habitant " + address + ", ");
+        bornCity.display();
+    }
+
+    @Override
+    public String toString() {
+        return "[lastName=" + lastName + ", firstName=" + firstName + ", age=" + age + ", address=" + (address==null?"unknown":address) + ", Ville=" + bornCity + "]";
+    }
+}
+
