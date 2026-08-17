baad4daa25a5406f6ef5a6e837812047de8f2bf1
Samuel Curran <samuel.curran@fms-ea.com>
2026-08-17 10:33:42 +0200

    Ajout des getters et setters dans ma classe ville

--- PooEx1Object/src/City.java
+++ PooEx1Object/src/City.java
@@ -3,7 +3,7 @@ public class City {
     private String country;
     private int nbInhabitant;
 
-    public City(String city, String country, int nbInhabitant){
+    public City(String city, String country, int nbInhabitant) {
         this.city = city;
         this.country = country;
         this.nbInhabitant = nbInhabitant;
@@ -12,5 +12,32 @@ public class City {
     @Override
     public String toString() {
         return "Ville: " + city + ", Pays: " + country + ", Nombre d'habitants: " + nbInhabitant;
+    }
+
+    /*
+        Getter et Setter
+     */
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getNbInhabitant() {
+        return nbInhabitant;
+    }
+
+    public void setNbInhabitant(int nbInhabitant) {
+        this.nbInhabitant = nbInhabitant;
     }
 }
