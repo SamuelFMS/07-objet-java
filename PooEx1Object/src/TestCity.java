0596d18f86059e70b28dc9099f5404121e59da2c
Samuel Curran <samuel.curran@fms-ea.com>
2026-08-17 10:30:55 +0200

    Test de ma classe Vile

--- PooEx1Object/src/TestCity.java
+++ PooEx1Object/src/TestCity.java
@@ -2,6 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class TestCity {
     public static void main(String[] args) {
-
+        City toulouse = new City("Toulouse", "France", 450000);
+        City bayonne = new City("Bayonne", "France", 54000);
+        City paris = new City("Paris", "France",  2100000);
+        City dublin = new City("Dublin", "Ireland",  592000);
+        System.out.println(toulouse);
+        System.out.println(bayonne);
     }
 }
