a8c6ebda60ca838ede4fb9765182552d7929f054
Samuel Curran <samuel.curran@fms-ea.com>
2026-08-17 10:44:50 +0200

    Fichier de tests de ma classe city

--- PooEx1Object/src/TestCity.java
+++ PooEx1Object/src/TestCity.java
@@ -6,9 +6,15 @@ public class TestCity {
         City bayonne = new City("Bayonne", "France", 54000);
         City paris = new City("Paris", "France",  2100000);
         City dublin = new City("Dublin", "Ireland",  592000);
+        toulouse.setNbInhabitant(-100);
         System.out.println(toulouse);
         System.out.println(bayonne);
+        System.out.println("Toulouse a eu 20 000 nouveau arrivants");
         toulouse.setNbInhabitant(toulouse.getNbInhabitant() + 20000);
+        System.out.println(toulouse);
+        System.out.println("Toulouse a 470 000 habitant mais on retire 100 000");
+        toulouse.setNbInhabitant(470000);
+        toulouse.setNbInhabitant(toulouse.getNbInhabitant() - 100000);
         System.out.println(toulouse);
     }
 }
