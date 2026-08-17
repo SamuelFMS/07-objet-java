b3a61a003d9855a851be0b3d80af81e148301352
Samuel Curran <samuel.curran@fms-ea.com>
2026-08-17 14:30:56 +0200

    Ajout doc string pour l'interface

--- Exercice4-AuBonMarche/src/Consumable.java
+++ Exercice4-AuBonMarche/src/Consumable.java
@@ -1,7 +1,23 @@
 import java.time.LocalDate;
 
 public interface Consumable {
+    /**
+     * Returns true if the product is at its ideal stage of maturity for consumption.
+     * @return
+     */
     boolean isRipe();
+
+    /**
+     * Calculates and returns true if the verification date is later than the product's use-by date.
+     * @param dateVerification
+     * @return
+     */
     boolean isExpired(LocalDate dateVerification);
+
+    /**
+     * Returns the number of days remaining until the use-by date.
+     * @param dateVerification
+     * @return
+     */
     long daysRemainingBeforeExpiration(LocalDate dateVerification);
 }
