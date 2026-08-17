7540858515979a0487c6c9b207d6c45f00c4c404
Samuel Curran <samuel.curran@fms-ea.com>
2026-08-17 13:37:18 +0200

    Minimum CA et salaire

--- PooEx3Heritage/src/fr/fms/entities/Commercial.java
+++ PooEx3Heritage/src/fr/fms/entities/Commercial.java
@@ -1,13 +1,21 @@
 package fr.fms.entities;
 
 public class Commercial extends Person {
+    public static final double minimumRemuneration = 0.01;
     private String companyName;
     private double remuneration;
 
     public Commercial(String lastName, String firstName, int age, String address, City bornCity, String companyName, double remuneration) {
         super(lastName, firstName, age, address, bornCity);
         this.companyName = companyName;
-        this.remuneration = remuneration;
+        this.setRemuneration(remuneration);
+    }
+
+    public void setRemuneration(double remuneration){
+        if(remuneration < minimumRemuneration){
+            System.out.println("% du chiffre d'affaire inférieur à " + minimumRemuneration + " impossible");
+        }
+        this.remuneration = Math.max(remuneration, minimumRemuneration);
     }
 
     public String toString() {
--- PooEx3Heritage/src/fr/fms/entities/Employee.java
+++ PooEx3Heritage/src/fr/fms/entities/Employee.java
@@ -1,31 +1,39 @@
 package fr.fms.entities;
 
 public class Employee extends Person {
+    public static final double minimumSalary = 1;
     private String companyName;
     private double salary;
 
     public Employee(String lastName, String firstName, int age, String address, City bornCity, String companyName, double salary) {
         super(lastName, firstName, age, address, bornCity);
         this.companyName = companyName;
-        this.salary = salary;
+        this.setSalary(salary);
     }
 
     public Employee(String lastName, String firstName, int age, String address, String companyName, double salary) {
         super(lastName, firstName, age, address);
         this.companyName = companyName;
-        this.salary = salary;
+        this.setSalary(salary);
     }
 
     public Employee(String lastName, String firstName, int age, String companyName, double salary) {
         super(lastName, firstName, age);
         this.companyName = companyName;
-        this.salary = salary;
+        this.setSalary(salary);
     }
 
     public Employee(String lastName, String firstName, String companyName, double salary) {
         super(lastName, firstName);
         this.companyName = companyName;
-        this.salary = salary;
+        this.setSalary(salary);
+    }
+
+    public void setSalary(double salary){
+        if(salary < minimumSalary){
+            System.out.println("Salaire inferieur à " + minimumSalary + " impossible");
+        }
+        this.salary = Math.max(salary, minimumSalary);
     }
 
     @Override
