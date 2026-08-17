cd49267d83c2119823ba998ea9c720b43c20848a
Samuel Curran <samuel.curran@fms-ea.com>
2026-08-17 13:08:44 +0200

    Creation de la classe employee qui herite de Person

---
+++ PooEx3Heritage/src/fr/fms/entities/Employee.java
@@ -0,0 +1,35 @@
+package fr.fms.entities;
+
+public class Employee extends Person {
+    private String companyName;
+    private double salary;
+
+    public Employee(String lastName, String firstName, int age, String address, City bornCity, String companyName, double salary) {
+        super(lastName, firstName, age, address, bornCity);
+        this.companyName = companyName;
+        this.salary = salary;
+    }
+
+    public Employee(String lastName, String firstName, int age, String address, String companyName, double salary) {
+        super(lastName, firstName, age, address);
+        this.companyName = companyName;
+        this.salary = salary;
+    }
+
+    public Employee(String lastName, String firstName, int age, String companyName, double salary) {
+        super(lastName, firstName, age);
+        this.companyName = companyName;
+        this.salary = salary;
+    }
+
+    public Employee(String lastName, String firstName, String companyName, double salary) {
+        super(lastName, firstName);
+        this.companyName = companyName;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Entreprise : " + companyName + ", Salaire: " + salary;
+    }
+}
