5feb132dc210034df6d6c89cc3417d2ae472d095
Samuel Curran <samuel.curran@fms-ea.com>
2026-08-18 15:10:34 +0200

    Stock format pour l'affichage

--- Exercice4-AuBonMarche/src/ProductBasket.java
+++ Exercice4-AuBonMarche/src/ProductBasket.java
@@ -1,10 +1,26 @@
 public class ProductBasket {
     private final Product product;
-    private final double quantity;
+    private double quantity;
 
     public ProductBasket(Product product, double quantity) {
         this.product = product;
         this.quantity = quantity;
+    }
+
+    public String stockFormat() {
+        String s;
+        switch (product.getUnite()) {
+            case "kg":
+                s = this.quantity + " kg";
+                break;
+            case "pcs":
+                s = this.quantity + " pièces";
+                break;
+            default:
+                s = this.quantity + product.getUnite();
+                break;
+        }
+        return s;
     }
 
     public double getPrice() {
@@ -22,5 +38,9 @@ public class ProductBasket {
 
     public double getQuantity() {
         return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
     }
 }
