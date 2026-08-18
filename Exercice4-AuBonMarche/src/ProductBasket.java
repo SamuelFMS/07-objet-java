85783aef27b9fb9f1278ea1da664a3cfc743e618
Samuel Curran <samuel.curran@fms-ea.com>
2026-08-18 11:06:18 +0200

    Classe ProductBasket

---
+++ Exercice4-AuBonMarche/src/ProductBasket.java
@@ -0,0 +1,18 @@
+public class ProductBasket {
+    private final Product product;
+    private final double quantity;
+
+    public ProductBasket(Product product, double quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return product.realPrice() * quantity;
+    }
+
+    @Override
+    public String toString() {
+        return product.getName() + " | " + quantity + " | " + product.realPrice() * quantity + "€";
+    }
+}
