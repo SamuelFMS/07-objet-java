09f55236ce021d748e0a5c3ea9f9d0c49c6a9dbd
Samuel Curran <samuel.curran@fms-ea.com>
2026-08-18 15:09:33 +0200

    Changement du camion pour quil rentre dans le ticket

--- Exercice4-AuBonMarche/src/DessinAscii.java
+++ Exercice4-AuBonMarche/src/DessinAscii.java
@@ -1,19 +1,20 @@
 public interface DessinAscii {
-    static void displayBin(){
-        System.out.println(" \\  ___________  /");
-        System.out.println("  \\ | | | | | | /");
-        System.out.println("   | | | | | | |");
-        System.out.println("   | | | | | | |");
-        System.out.println("   | | | | | | |");
-        System.out.println("   |___________|");
-    }
+    static void displayTruck(Basket deliveryBasket){
 
-    static void displayTruck(){
-        System.out.println("    ____________________________________________");
-        System.out.println("   |                                            | ");
-        System.out.println("   |                LIVRAISON                   | ___");
-        System.out.println("   |                                            | |  \\");
-        System.out.println("   |____________________________________________|_|___\\");
-        System.out.println("   `---(o)(o)-------------------------------------(o)--'");
+
+        System.out.println("|   ___________________________________            |");
+        System.out.println("|  |           LIVRAISON               |           |");
+        int number = 0;
+        for(ProductBasket productBasket : deliveryBasket.getMyBasket()) {
+            if(number != deliveryBasket.getMyBasket().size()-1){
+                System.out.println("|  |  "+TextFormatter.formatSize(productBasket.getProduct().getName() + " ->  " + productBasket.stockFormat(),33)+"|           |");
+            } else {
+                System.out.println("|  |  "+TextFormatter.formatSize(productBasket.getProduct().getName() + " ->  " + productBasket.stockFormat(),33)+"| ___       |");
+            }
+            number++;
+        }
+        System.out.println("|  |                                   | |  \\      |");
+        System.out.println("|  |___________________________________|_|___\\     |");
+        System.out.println("|  `---(o)(o)-----------------------------(o)-     |");
     }
 }
