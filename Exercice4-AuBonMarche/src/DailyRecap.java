e3225634aacb44f44d44aed7fe1a0e14be8f9982
Samuel Curran <samuel.curran@fms-ea.com>
2026-08-18 13:51:18 +0200

    Daily recap take basket for lost and gain

--- Exercice4-AuBonMarche/src/DailyRecap.java
+++ Exercice4-AuBonMarche/src/DailyRecap.java
@@ -2,13 +2,13 @@ import java.time.LocalDate;
 
 public final class DailyRecap {
     private static LocalDate currentDate;
-    private static double numberSales;
-    private static double numberLost;
+    private static Basket sales;
+    private static Basket lost;
 
     public static void initDay(LocalDate currentDate){
         DailyRecap.currentDate = currentDate;
-        numberSales = 0;
-        numberLost = 0;
+        sales = new Basket();
+        lost = new Basket();
     }
 
     public static void displayCustomerSummaryOfTheDay() {
@@ -17,28 +17,33 @@ public final class DailyRecap {
         System.out.println("|  RECAPITULATIF JOURNALIER : CHIFFRE D'AFFAIRES   |");
         System.out.println("|__________________________________________________|");
         System.out.println("|                                                  |");
-        System.out.println("|  "+ FormatOrTruncate.padOrTruncate(("Date : " + currentDate), 47) + " |");
+        System.out.println("|  "+ TextFormatter.formatSize(("Date : " + currentDate), 47) + " |");
         System.out.println("|                                                  |");
-        System.out.println("|  " + FormatOrTruncate.padOrTruncate("Ventes Brutes : " + numberSales,47) + " |");
-        System.out.println("|  " + FormatOrTruncate.padOrTruncate("Pertes (Fruits expirés) : " + (0-numberLost),47) + " |");
+        System.out.println("|  " + TextFormatter.formatSize("Ventes Brutes : " + sales.getPriceTotalBasket() + " €",47) + " |");
+        for(ProductBasket salesProduct : sales.getMyBasket()) {
+            System.out.println("|  " + TextFormatter.formatSize("• ["+salesProduct.getProduct().getName()+"]  ->  "+salesProduct.getQuantity()*salesProduct.getProduct().realPrice()+" €",47) + " |");
+        }
+        System.out.println("|  " + TextFormatter.formatSize("Pertes (Fruits expirés) : " + (0-lost.getPriceTotalBasket()) + " €",47) + " |");
+        for(ProductBasket lostProduct : lost.getMyBasket()) {
+            System.out.println("|  " + TextFormatter.formatSize("• ["+lostProduct.getProduct().getName()+"]  ->  -"+lostProduct.getQuantity()*lostProduct.getProduct().realPrice()+" €",47) + " |");
+        }
         System.out.println("|                                                  |");
-        System.out.println("|  " + FormatOrTruncate.padOrTruncate("Chiffre d'affaires net: "+ (numberSales - numberLost) ,47) + " |");
+        System.out.println("|  " + TextFormatter.formatSize("Chiffre d'affaires net: "+ (sales.getPriceTotalBasket() - lost.getPriceTotalBasket())+ " €" ,47) + " |");
         System.out.println("|__________________________________________________|");
+    }
+
+    public static void addToLost(Product product, double quantity) {
+        lost.addToBasket(product, quantity);
+    }
+
+    public static void addToGain(Product product, double quantity) {
+        sales.addToBasket(product, quantity);
     }
 
     /*
         Setter
      */
-
     public static void setCurrentDate(LocalDate currentDate) {
         DailyRecap.currentDate = currentDate;
-    }
-
-    public static void setNumberSales(double numberSales) {
-        DailyRecap.numberSales = numberSales;
-    }
-
-    public static void setNumberLost(double numberLost) {
-        DailyRecap.numberLost = numberLost;
     }
 }
