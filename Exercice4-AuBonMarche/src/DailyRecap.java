1cb426171baa5eeedf46392de035e483e132e364
Samuel Curran <samuel.curran@fms-ea.com>
2026-08-18 15:10:17 +0200

    Ajoute la livraison

--- Exercice4-AuBonMarche/src/DailyRecap.java
+++ Exercice4-AuBonMarche/src/DailyRecap.java
@@ -4,11 +4,13 @@ public final class DailyRecap {
     private static LocalDate currentDate;
     private static Basket sales;
     private static Basket lost;
+    private static Basket delivery;
 
     public static void initDay(LocalDate currentDate){
         DailyRecap.currentDate = currentDate;
         sales = new Basket();
         lost = new Basket();
+        delivery = new Basket();
     }
 
     public static void displayCustomerSummaryOfTheDay() {
@@ -16,9 +18,16 @@ public final class DailyRecap {
         System.out.println("|                                                  |");
         System.out.println("|  RECAPITULATIF JOURNALIER : CHIFFRE D'AFFAIRES   |");
         System.out.println("|__________________________________________________|");
-        System.out.println("|                                                  |");
         System.out.println("|  "+ TextFormatter.formatSize(("Date : " + currentDate), 47) + " |");
-        System.out.println("|                                                  |");
+        System.out.println("|__________________________________________________|");
+
+        if(!delivery.getMyBasket().isEmpty()){
+            DessinAscii.displayTruck(delivery);
+            System.out.println("|__________________________________________________|");
+        } else {
+            System.out.println("|                                                  |");
+        }
+
         System.out.println("|  " + TextFormatter.formatSize("Ventes Brutes : " + sales.getPriceTotalBasket() + " €",47) + " |");
         for(ProductBasket salesProduct : sales.getMyBasket()) {
             System.out.println("|  " + TextFormatter.formatSize("• ["+salesProduct.getProduct().getName()+"]  ->  "+salesProduct.getQuantity()*salesProduct.getProduct().realPrice()+" €",47) + " |");
@@ -38,6 +47,10 @@ public final class DailyRecap {
 
     public static void addToGain(Product product, double quantity) {
         sales.addToBasket(product, quantity);
+    }
+
+    public static void addToDelivery(Product product, double quantity) {
+        delivery.addToBasket(product, quantity);
     }
 
     /*
