0dec63da3c64f2a175e502341cac689f771b5e24
Samuel Curran <samuel.curran@fms-ea.com>
2026-08-18 11:06:04 +0200

    Classe Panier

---
+++ Exercice4-AuBonMarche/src/Basket.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class Basket {
+    private final ArrayList<ProductBasket> myBasket = new ArrayList<>();
+
+    public void addToBasket(Product product, double quantity) {
+        myBasket.add(new ProductBasket(product, quantity));
+        product.setStockQuantity(product.getStockQuantity() - quantity);
+    }
+
+    public double getPriceTotalBasket() {
+        double total = 0;
+        for (ProductBasket productBasket : myBasket) {
+            total += productBasket.getPrice();
+        }
+        return total;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        if (myBasket.isEmpty()) {
+            res.append("Mon panier de basket est vide");
+        } else {
+            for (ProductBasket productBasket : myBasket) {
+                res.append(productBasket).append("\n");
+            }
+            res.append("Total: ").append(getPriceTotalBasket()).append("€");
+        }
+        return res.toString();
+    }
+}
