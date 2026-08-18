3ab14987143d2b2ebb8c0ef815c9f40e0ca1fa36
Samuel Curran <samuel.curran@fms-ea.com>
2026-08-18 11:34:15 +0200

    Dessin ascii

---
+++ Exercice4-AuBonMarche/src/DessinAscii.java
@@ -0,0 +1,19 @@
+public class DessinAscii {
+    public static void displayBin(){
+        System.out.println(" \\  ___________  /");
+        System.out.println("  \\ | | | | | | /");
+        System.out.println("   | | | | | | |");
+        System.out.println("   | | | | | | |");
+        System.out.println("   | | | | | | |");
+        System.out.println("   |___________|");
+    }
+
+    public static void displayTruck(){
+        System.out.println("    ____________________________________________");
+        System.out.println("   |                                            | ");
+        System.out.println("   |                LIVRAISON                   | ___");
+        System.out.println("   |                                            | |  \\");
+        System.out.println("   |____________________________________________|_|___\\");
+        System.out.println("   `---(o)(o)-------------------------------------(o)--'");
+    }
+}
