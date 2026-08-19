943f6e7f2e281ea5c524b747064eb65a49bfc68c
Samuel Curran <samuel.curran@fms-ea.com>
2026-08-19 15:10:34 +0200

    formatString String appendText pour choisir le char que lon souhaite

--- Exercice4-AuBonMarche/src/TextFormatter.java
+++ Exercice4-AuBonMarche/src/TextFormatter.java
@@ -1,6 +1,7 @@
 public interface TextFormatter {
     /**
      * Truncate or append a text
+     *
      * @param text
      * @param length
      * @return
@@ -9,8 +10,7 @@ public interface TextFormatter {
         String safeText = (text == null) ? "" : text;
         if (safeText.length() > length) {
             safeText = safeText.substring(0, length);
-        }
-        else {
+        } else {
             StringBuilder res = new StringBuilder();
             res.append(safeText);
             while (res.length() < length) {
@@ -22,11 +22,33 @@ public interface TextFormatter {
     }
 
     /**
+     * Truncate or append a text with appendText
+     *
+     * @param text
+     * @param length
+     * @return
+     */
+    static String formatSize(String text, int length, String appendText) {
+        String safeText = (text == null) ? "" : text;
+        if (safeText.length() > length) {
+            safeText = safeText.substring(0, length);
+        } else {
+            StringBuilder res = new StringBuilder();
+            res.append(safeText);
+            while (res.length() < length) {
+                res.append(appendText);
+            }
+            safeText = res.toString();
+        }
+        return safeText;
+    }
+
+    /**
      * Repeat a String x times
      */
-    static String repeat(String a, int x){
+    static String repeat(String a, int x) {
         StringBuilder res = new StringBuilder();
-        for(int i = 0; i<x;i++){
+        for (int i = 0; i < x; i++) {
             res.append(a);
         }
         return res.toString();
